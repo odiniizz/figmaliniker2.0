89d503eff8afa70c0922a622feeaa0da53e4f133
odiniizz <jcesarddiniz05@gmail.com>
2023-11-29 17:26:47 -0300

    j

--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -14,13 +14,13 @@ export const Section = styled.section`
     font-size: 2rem;
   }
   form {
-    background-color: ${colors.thirdLight};
-    border: 0.2rem solid ${colors.third};
+    background-color: ${colors.white};
+    border: 0.2rem solid ${colors.black};
     border-radius: 1rem;
     padding: 1.5rem;
     div {
       background-color: ${colors.white};
-      border: 0.15rem solid ${colors.third};
+      border: 0.15rem solid ${colors.black};
       border-radius: 0.6rem;
       display: flex;
       margin: 0.5rem 0;
@@ -28,7 +28,7 @@ export const Section = styled.section`
         height: 3rem;
         width: 3rem;
         padding: 0.3rem;
-        color: ${colors.third};
+        color: ${colors.black};
       }
       input {
         width: 100%;
